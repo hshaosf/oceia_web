dbe6b9a760fd9ba652a59276af5aba6f6b0b4245
Hui Shao <huiran.shao@sfgov.org>
2017-07-11 22:03:27 +0000

    update results template

--- modules/custom/ccsf_oceia/src/Controller/OceiaController.php
+++ modules/custom/ccsf_oceia/src/Controller/OceiaController.php
@@ -6,7 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 /**
  * Page controller.
  */
-class PageController extends ControllerBase {
+class OceiaController extends ControllerBase {
 
   /**
    * {@inheritdoc}
@@ -19,7 +19,7 @@ class PageController extends ControllerBase {
     }
     switch($name){
       case 'home' : 
-        $vars['results'] = $this->getResults(); 
+        $vars['results'] = self::getResults(); 
         break;
     }
     return [
@@ -29,7 +29,19 @@ class PageController extends ControllerBase {
     ];
   }
 
-  protected function getResults(){
+  public static function get($input){
+    $parts = explode('.', $input); 
+    if(count($parts) > 1){
+      $method = 'get'.ucwords($parts[1]);
+      if(method_exists(get_class(), $method)){
+        return self::$method(); 
+      } 
+    }
+    error_log(get_class().'.get:'.$input.'('.$method.') not found'); 
+    return; 
+  }
+
+  protected static function getResults(){
     $results = array(); 
     $result_nodes = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(array('type' => 'result_page')); 
     $lang_code = \Drupal::service('language_manager')->getCurrentLanguage()->getId();
@@ -39,12 +51,26 @@ class PageController extends ControllerBase {
         if($node->hasTranslation($lang_code)){
           $node = $node->getTranslation($lang_code); 
         }
+        $nid = $node->id(); 
         $title = $node->get('title')->value;
         $url = $node->url();
-        $results[] = array('title'=>$title, 'url'=>$url); 
+        $results[] = array_merge(array('nid'=>$nid, 'title'=>$title, 'url'=>$url), self::getResultExtra($nid)); 
       }
     }
     return $results; 
   }
+
+  protected static function getResultExtra($nid){
+    $extra = array(); 
+    switch($nid){
+      case 19 : default : 
+        $extra['cta_class'] = 'bg-primary-tint';
+        $extra['fa_icon'] = 'fa-id-card-o';
+        break;
+    }
+    return $extra; 
+  }
+
+
 
 }
