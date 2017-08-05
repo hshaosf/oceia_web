3fa6b1ef2c92712406b89e9a9b0fa038dcca7a94
Hui Shao <huiran.shao@sfgov.org>
2017-08-05 00:17:33 +0000

    add browser back function for html5 browsers

--- themes/custom/oceia/js/oceia.js
+++ themes/custom/oceia/js/oceia.js
@@ -10,6 +10,14 @@
     clearStorage : function(){ if(this.supportLocalStorage()){return _w.localStorage.clear();}return false;},
     set : function(key,value){ _v[key] = value;},
     get : function(key){return _v[key];},
+    supportHistory : function(){ 
+      return (this.supportLocalStorage() && history.pushState);
+    },
+    pushHistory : function(state, title, url){ 
+      if(this.supportHistory()){ 
+        if(!title){ title = _d.title; }
+        history.pushState(state, title, url); 
+      }}
   }
   var oceia = {
     msg : {
@@ -21,53 +29,105 @@
         el.after('<div class="message is-reserved oceia-message"><span class="message-text">'+(m?m:'Error')+'</span></div>'); 
       }
     },
-    qs : function(){
-      var _t = this, hide = function(){$('.question').hide(); }
-      $('.question button[data-next]').click(function(e){
-        var el = $(this), name = el.data('name'), next = el.data('next'), value = false; 
-        _t.msg.clear();
-        
-        if(name){
-          var selected = [], input = $('.question *[name="'+name+'"]');
-          if(input.length){ 
-            if(input.attr('type')=='checkbox' || input.attr('type')=='radio'){
-              input.map(function(i,e){
-                if($(e).is(':checked')){ 
-                  value = $(e).val();
-                  selected.push($(e).val())
-                }}); 
+    qs : {
+      init : function(){
+        var _t = this, _o = oceia;
+        _t.first = $('.question:first').attr('id');
+        $('.question button[data-next]').click(function(e){
+          var el = $(this), name = el.data('name'), next = el.data('next'), value = false; 
+          _o.msg.clear();
+          
+          if(name){
+            var selected = [], input = $('.question *[name="'+name+'"]');
+            if(input.length){ 
+              if(input.attr('type')=='checkbox' || input.attr('type')=='radio'){
+                input.map(function(i,e){
+                  if($(e).is(':checked')){ 
+                    value = $(e).val();
+                    selected.push($(e).val())
+                  }}); 
+              }else{
+                value = input.val();
+                selected.push(input.val());
+              }
+              if(!selected.length){
+                _o.msg.display((el.data('error')?el.data('error'):'Required'),el.closest('.question').find('div:not(.form-back)').first());
+                return;
+              }
+            }
+          }
+
+          if(next){ 
+            next = (next=='value' && value !== false)?value:next;
+            if(next.substring(0,1).match(/[a-zA-Z]/) && $('#'+next).length){
+              _t.goto(next);
             }else{
-              value = input.val();
-              selected.push(input.val());
+              if(next){
+                _t.setLast(_t.current);
+                _w.location.href=next;
+              } 
             }
-            if(!selected.length){
-              _t.msg.display((el.data('error')?el.data('error'):'Required'),el.closest('.question').find('div:not(.form-back)').first());
-              return;
-            }
+            return; 
           }
+        }); 
+
+        $('.question .form-back a').click(function(e){
+          e.preventDefault(); 
+          var el = $(this), back = el.data('href');
+          _t.goto(back, true);
+        });
+
+        if(cmn.supportHistory() && _t.getLast()){
+          _t.load(_t.getLast())
         }
 
-        if(next){ 
-          next = (next=='value' && value !== false)?value:next;
-          if(next.substring(0,1).match(/[a-zA-Z]/) && $('#'+next).length){
-            hide(); $('#'+next).show(); 
-          }else{
-            if(next){
-              _w.location.href=next;
-            } 
-          }
-          return; 
+      },
+      hide : function(){
+        $('.question').hide();
+      },
+      push : function(q){
+        cmn.pushHistory({oceia_qs:q});
+      },
+      goto : function(p, is_back){
+        var _t = this;
+        if($('#'+p).length){
+            if(!is_back && cmn.supportHistory()){
+              _t.push(p); 
+              _t.current = p;
+            }
+            if(is_back && cmn.supportHistory()){
+              history.back();
+            }else{
+              _t.load(p);
+            }
+            
+            return true;
         }
-      }); 
-
-      $('.question .form-back a').click(function(e){
-        e.preventDefault(); 
-        var el = $(this), back = el.data('href');
-        if($('#'+back).length){
-          hide(); $('#'+back).show(); 
+        return false;
+      },
+      loadState : function(e){
+        var s = this.first;
+        if(e && e.oceia_qs){
+          s = e.oceia_qs;
         }
-      });
-      return this; 
+        this.load(s);
+      },
+      load : function(p){
+        if($('#'+p).length){
+          this.hide(); $('#'+p).show();
+        }
+      },
+      setLast : function(l){
+        if(cmn.supportHistory){
+          cmn.setItem('last_qs', l);
+        }
+      },
+      getLast : function(){
+        return cmn.getItem('last_qs');
+      },
+      clearLast : function(){
+        cmn.setItem('last_qs', '');
+      }
     },
     checkbox : function(id, item, field, alert){
       var el = $('#'+id),
@@ -172,8 +232,28 @@
       return this;
     },
     ready : function(){ 
-      console.log('ready');
-      this.menu().tooltip($('.translate-bar')).qs().toggles().gotoSelect().checkbox('results-languages', '.block-item-resources', '.field-languages', '#results-languages-alert'); 
+      console.log('This site is in ALPHA. We\'re just getting started! We\'d love to get your feedback.');
+      var _t = this;
+      _t.menu().tooltip($('.translate-bar')).toggles().gotoSelect().checkbox('results-languages', '.block-item-resources', '.field-languages', '#results-languages-alert'); 
+
+      if($('#oceia-help').length){
+        _t.qs.init();
+        cmn.setItem('last_page', 'help');
+        _t.qs.clearLast();
+      }else{
+        if(!($('body').hasClass('page-node-type-result-page') 
+            && cmn.getItem('last_page')=='help')){
+          _t.qs.clearLast();
+        }
+        cmn.setItem('last_page', 'other');
+      }
+
+      _w.onpopstate = function(e){
+        if($('#oceia-help').length){
+          _t.qs.loadState(e.state);
+        }  
+      }
+      
     }
   }; 
   $(document).ready(function(){oceia.ready()});
