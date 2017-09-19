5faf62e016288074046fe36c5377b39ef6366099
hui <hui@webengieer1smbp.net-10-1-15.dhcp.sfgov.org>
2017-09-18 17:52:21 -0700

    domain redirects

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,17 +1,19 @@
 <?php
 
 if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'www.immigrants.sfgov.org') {
-  $redirect = 'http://immigrants.sfgov.org';
+  $redirect = 'https://immigrants.sfgov.org';
   header('HTTP/1.1 301 Moved Permanently');
   header('Location: ' . $redirect);
   exit();
 }
 
-if (isset($_SERVER['HTTPS']) &&
-    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
-    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if ((!isset($_ENV['PANTHEON_ENVIRONMENT']) || ($_ENV['PANTHEON_ENVIRONMENT'] == 'live')) &&
+    php_sapi_name() != 'cli' && 
+   (!isset($_SERVER['HTTPS']) &&
+    !($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) &&
+    !(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))) {
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
   header('HTTP/1.1 301 Moved Permanently');
   header('Location: ' . $redirect);
   exit();
